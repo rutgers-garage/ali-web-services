0edee737cf3a666ddb10beec80ae8436b8f4082a
Ali Mohamad <alimohamad1122@gmail.com>
2021-04-12 23:57:24 -0400

    cleaned up child directory

--- AWS-Child/awsChild.go
+++ AWS-Child/awsChild.go
@@ -1,31 +1,5 @@
-// exec("docker run <name>") + docker stop + docker stat
-// edit /etc/nginx/nginx.conf
-// restart nginx
-
 package main
-
-import (
-	"fmt"
-	"log"
-	"os/exec"
-)
 
 func main() {
 	startService("https://hemangandhi.github.io")
-}
-
-func startService(container string) {
-	prg := "open"
-	//arg1 := "pns"
-	// declare variable for port flag
-	// declare variable for port fowarding
-
-	cmd := exec.Command(prg, container) // add port flag + value
-	stdout, err := cmd.Output()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Print(string(stdout))
-
 }
---
+++ AWS-Child/containerManager.go
@@ -0,0 +1,27 @@
+// exec("docker run <name>") + docker stop + docker stat
+// edit /etc/nginx/nginx.conf
+// restart nginx
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"os/exec"
+)
+
+func startService(container string) {
+	prg := "open"
+	//arg1 := "pns"
+	// declare variable for port flag
+	// declare variable for port fowarding
+
+	cmd := exec.Command(prg, container) // add port flag + value
+	stdout, err := cmd.Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Print(string(stdout))
+
+}
